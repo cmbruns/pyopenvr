91bfb2d9de8785a11be8aaa573ca9c480a0dd015
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-05-07 09:15:47 -0400

    Arrange files into openvr package structure.

---
+++ openvr/__init__.py
@@ -0,0 +1,58 @@
+#!/bin/env python
+
+import os
+from ctypes import *
+
+# Add current directory to PATH, so we can load the DLL from right here.
+os.environ['PATH'] = os.path.dirname(__file__) + ';' + os.environ['PATH']
+_openvr = cdll.openvr_api
+
+ENUM_TYPE = c_int32
+
+EVRInitError = ENUM_TYPE
+
+
+EVRApplicationType = ENUM_TYPE
+EVRApplicationType_VRApplication_Other = 0
+EVRApplicationType_VRApplication_Scene = 1
+EVRApplicationType_VRApplication_Overlay = 2
+EVRApplicationType_VRApplication_Background = 3
+EVRApplicationType_VRApplication_Utility = 4
+EVRApplicationType_VRApplication_VRMonitor = 5
+
+
+_openvr.VR_InitInternal.restype = POINTER(c_int)
+_openvr.VR_InitInternal.argtypes = [POINTER(EVRInitError), EVRApplicationType]
+def vr_init():
+    error_result = EVRInitError()
+    result = _openvr.VR_InitInternal(byref(error_result), EVRApplicationType_VRApplication_Scene)
+    print error_result
+
+_openvr.VR_IsHmdPresent.restype = c_bool
+_openvr.VR_IsHmdPresent.argtypes = []
+def is_hmd_present():
+    return _openvr.VR_IsHmdPresent()
+
+_openvr.VR_ShutdownInternal.restype = None
+_openvr.VR_ShutdownInternal.argtypes = []
+def vr_shutdown():
+    _openvr.VR_ShutdownInternal()
+
+
+class IVRSystem:
+    def __init__(self):
+        self.ptr = vr_init()
+
+    def shutdown():
+        vr_shutdown()
+        self.ptr = None
+
+
+print "Hello"
+
+if not is_hmd_present():
+    sys.exit(0)
+
+vr_init()
+vr_shutdown()
+
