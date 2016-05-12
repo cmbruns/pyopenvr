1377a3b948b68f0f5e2171aaaeef264dc277ebd6
Christopher Bruns <brunsc@janelia.hhmi.org>
2016-05-12 09:57:04 -0400

    Enhance hello example to work with python 3.

--- src/examples/hello.py
+++ src/examples/hello.py
@@ -4,25 +4,25 @@ import sys
 import time
 import openvr
 
-print "OpenVR test program"
+print ("OpenVR test program")
 
 if openvr.isHmdPresent():
-    print "VR head set found"
+    print ("VR head set found")
 
 if openvr.isRuntimeInstalled():
-    print "Runtime is installed"
+    print ("Runtime is installed")
 
 vr_system = openvr.init(openvr.VRApplication_Scene)
 
-print openvr.runtimePath()
+print (openvr.runtimePath())
 
-print vr_system.getRecommendedRenderTargetSize()
+print (vr_system.getRecommendedRenderTargetSize())
 
-print vr_system.isDisplayOnDesktop()
+print (vr_system.isDisplayOnDesktop())
 
-for i in range(100):
+for i in range(10):
     xform = vr_system.getEyeToHeadTransform(openvr.Eye_Left)
-    print xform
+    print (xform)
     sys.stdout.flush()
     time.sleep(0.2)
 
