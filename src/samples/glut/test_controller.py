e60a602917d29028fe9417049de148748937e941
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-06-16 09:03:27 -0400

    Include color cube in test_controller.py sample.

--- src/samples/glut/test_controller.py
+++ src/samples/glut/test_controller.py
@@ -5,6 +5,7 @@
 from glut_app import GlutApp
 from openvr.gl_renderer import OpenVrGlRenderer
 from test_controller_actor import TrackedDevicesActor
+from openvr.color_cube_actor import ColorCubeActor
 
 """
 Minimal glfw programming example which colored OpenGL cube scene that can be closed by pressing ESCAPE.
@@ -15,5 +16,6 @@ if __name__ == "__main__":
     renderer = OpenVrGlRenderer()
     actor = TrackedDevicesActor(renderer.poses)
     renderer.append(actor)
+    renderer.append(ColorCubeActor())
     with GlutApp(renderer, "Controller test") as glutApp:
         glutApp.run_loop()
