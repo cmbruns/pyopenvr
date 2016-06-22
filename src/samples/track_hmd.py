4cb225121392c4a6e2d9672fe18c8c9e627e913f
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-06-22 17:35:33 -0400

    Modify track_hmd.py to use VRCompositor() as mentioned in issue #17.

--- src/samples/track_hmd.py
+++ src/samples/track_hmd.py
@@ -4,15 +4,17 @@ import sys
 import time
 import openvr
 
-vr_system = openvr.init(openvr.VRApplication_Scene)
+openvr.init(openvr.VRApplication_Scene)
 
+# Create data structure to hold locations of tracked devices such as headsets, controllers, and lighthouses
+poses_t = openvr.TrackedDevicePose_t * openvr.k_unMaxTrackedDeviceCount
+poses = poses_t()
+
+# Print out headset transform five times a second for 20 seconds
 for i in range(100):
-    poses = vr_system.getDeviceToAbsoluteTrackingPose(
-        openvr.TrackingUniverseStanding,
-        0,
-        openvr.k_unMaxTrackedDeviceCount)
-    pose = poses[openvr.k_unTrackedDeviceIndex_Hmd]
-    print(pose.mDeviceToAbsoluteTracking)
+    openvr.VRCompositor().waitGetPoses(poses, len(poses), None, 0)
+    hmd_pose = poses[openvr.k_unTrackedDeviceIndex_Hmd]
+    print(hmd_pose.mDeviceToAbsoluteTracking)
     sys.stdout.flush()
     time.sleep(0.2)
 
