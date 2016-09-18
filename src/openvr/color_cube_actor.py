85b7f913d83830768dce09cb84766aa4e8d38879
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-09-18 11:57:26 -0400

    Update line number tag in color cube shader

--- src/openvr/color_cube_actor.py
+++ src/openvr/color_cube_actor.py
@@ -90,7 +90,7 @@ class ColorCubeActor(object):
         fragment_shader = compileShader(dedent(
             """\
             #version 450 core
-            #line 93
+            #line 94
             
             in vec3 _color;
             out vec4 FragColor;
