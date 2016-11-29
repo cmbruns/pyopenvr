a9798898f1a27d6faac47c5ed0b6ddddd634d454
Christopher Bruns <cmbruns@rotatingpenguin.com>
2016-11-29 11:31:51 -0500

    Add classifiers to setup.py Use consistent quoting style in setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,31 @@ from setuptools import setup
 exec(open('src/openvr/version.py').read())
 
 setup(
-    name = "openvr",
-    version = __version__,
-    author = "Christopher Bruns",
-    author_email = "cmbruns@rotatingpenguin.com",
-    description = "Valve OpenVR SDK python bindings using ctypes",
-    url = "https://github.com/cmbruns/pyopenvr",
-    download_url = "https://github.com/cmbruns/pyopenvr/tarball/" + __version__,
-    package_dir = {'': 'src'},
-    packages = ['openvr', 'openvr.glframework'],
-    package_data = {'openvr': ['*.dll', '*.so', '*.dylib']},
-    keywords = "openvr valve htc vive vr virtual reality",
-    classifiers = [],
+    name='openvr',
+    version=__version__,
+    author='Christopher Bruns and others',
+    author_email='cmbruns@rotatingpenguin.com',
+    description='Valve OpenVR SDK python bindings using ctypes',
+    url='https://github.com/cmbruns/pyopenvr',
+    download_url='https://github.com/cmbruns/pyopenvr/tarball/' + __version__,
+    package_dir={'': 'src'},
+    packages=['openvr', 'openvr.glframework'],
+    package_data={'openvr': ['*.dll', '*.so', '*.dylib']},
+    keywords='openvr valve htc vive vr virtual reality 3d graphics',
+    license='BSD',
+    classifiers="""\
+Environment :: Win32 (MS Windows)
+Intended Audience :: Developers
+License :: OSI Approved :: BSD License
+Operating System :: Microsoft :: Windows
+Operating System :: Microsoft :: Windows :: Windows 7
+Operating System :: POSIX :: Linux
+Operating System :: MacOS :: MacOS X
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: Implementation :: CPython
+Topic :: Multimedia :: Graphics :: 3D Rendering
+Topic :: Scientific/Engineering :: Visualization
+Development Status :: 4 - Beta
+""".splitlines(),
 )
