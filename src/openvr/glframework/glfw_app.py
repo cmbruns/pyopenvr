af5e6c8629054bd0ea8cfec926f71350fc9f2ffd
Christopher Bruns <cmbruns@rotatingpenguin.com>
2017-04-20 16:54:15 -0400

    Generalize GlfwApp to allow non-vr implementation.

--- src/openvr/glframework/glfw_app.py
+++ src/openvr/glframework/glfw_app.py
@@ -11,10 +11,10 @@ Toy glfw application for use with "hello world" examples demonstrating pyopenvr
 """
 
 
-class GlfwApp(object):
-    "GlfwApp uses glfw library to create an opengl context, listen to keyboard events, and clean up"
+class GlfwBaseApp(object):
+    "Base version renders to desktop"
 
-    def __init__(self, renderer, title="GLFW test"):
+    def __init__(self, renderer, title="GlfwBaseApp"):
         "Creates an OpenGL context and a window, and acquires OpenGL resources"
         self.renderer = renderer
         self.title = title
@@ -22,20 +22,20 @@ class GlfwApp(object):
         
         if not glfw.init():
             raise Exception("GLFW Initialization error")
-        # Get OpenGL 4.1 context
+        # Use modern OpenGL version 4.5 core
         glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 4)
-        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 1)
+        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 5)
         glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
-        # Double buffered screen mirror stalls VR headset rendering,
-        # So use single-buffering
-        glfw.window_hint(glfw.DOUBLEBUFFER, False)
+        self._configure_context()
         self.window = glfw.create_window(self.renderer.window_size[0], self.renderer.window_size[1], self.title, None, None)
         if self.window is None:
             glfw.terminate()
             raise Exception("GLFW window creation error")
         glfw.set_key_callback(self.window, self.key_callback)
         glfw.make_context_current(self.window)
-        glfw.swap_interval(0)
+
+    def _configure_context(self):
+        pass
 
     def __enter__(self):
         "setup for RAII using 'with' keyword"
@@ -49,7 +49,7 @@ class GlfwApp(object):
         if self._is_initialized:
             return # only initialize once
         glfw.make_context_current(self.window)
-        if self.renderer is not None:
+        if self.renderer:
             self.renderer.init_gl()
         self._is_initialized = True
 
@@ -64,10 +64,11 @@ class GlfwApp(object):
         glfw.poll_events()
 
     def dispose_gl(self):
-        if self.window is not None:
+        if self.window:
             glfw.make_context_current(self.window)
-            if self.renderer is not None:
+            if self.renderer:
                 self.renderer.dispose_gl()
+        glfw.destroy_window(self.window)
         glfw.terminate()
         self._is_initialized = False
 
@@ -80,3 +81,24 @@ class GlfwApp(object):
         "keep rendering until the user says quit"
         while not glfw.window_should_close(self.window):
             self.render_scene()
+
+
+class GlfwApp(GlfwBaseApp):
+    "GlfwApp uses glfw library to create an opengl context, listen to keyboard events, and clean up"
+
+    def __init__(self, renderer, title="GlfwApp"):
+        super(GlfwApp, self).__init__(renderer=renderer, title=title)
+        glfw.swap_interval(0)
+
+    def _configure_context(self):
+        # Double buffered screen mirror stalls VR headset rendering,
+        # So use single-buffering
+        glfw.window_hint(glfw.DOUBLEBUFFER, False)
+
+
+class GlfwVrApp(GlfwApp):
+    def __init__(self, actors=[], title="GlfwVrApp"):
+        import openvr.gl_renderer
+        renderer = openvr.gl_renderer.OpenVrGlRenderer(actor=actors, multisample=2)
+        super(GlfwVrApp, self).__init__(renderer=renderer)
+        glfw.swap_interval(0)
